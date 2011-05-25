3d7ea04a2b6044d5d3e8970669cac3ade02f2710
josh <josh@schulzone.org>
2011-05-25 05:43:54 +0000

    stopping for the night

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,37 +4,36 @@ var foursquare = require('node-foursquare').Foursquare();
 var CLIENTID = 'ALFZEBG4YWG2O11JLN2UHOEO0YEH2RFNOK0GLNYXCNEUT1GR';
 var CLIENTSECRET = 'FZGAIYKGDQRRYUYCRQ3L5YI14EU1MGLZAEF34EY3REVPQEP5';
 var LOGINREDIRECT = 'http://nodejs.flockasquare.dotcloud.com/fin';
+var CALLBACKURL = 'http://nodejs.flockasquare.dotcloud.com/callback';
 
 var app = express.createServer();
 
-/* Foursquare login crap 
+/* Foursquare login crap */
 app.get('/login', function(req, res){
-	var url = Foursquare.getAuthClientRedirectUrl(CLIENTID, LOGINREDIRECT);
+	var url = foursquare.getAuthClientRedirectUrl(CLIENTID, CALLBACKURL);
 	res.writeHead( 303, {"location": url});
 	res.end();
 });
 
 app.get('/callback', function(req, res){
 	var code = req.query.code;
-	Foursquare.getAccessToken({
+	foursquare.getAccessToken({
 		code: code,
-		redirect_urk: LOGINREDIRECT,
+		redirect_uri: LOGINREDIRECT,
 		client_id: CLIENTID,
 		client_secret: CLIENTSECRET
 	}, function (error, accessToken){
-			if(error){
-				res.send("An error was thrown: "+error.message);
-			} else {
-				//save and do something
-				res.cookie('token', accessToken);
-				res.writeHead(303, {"location": LOGINREDIRECT);
-			}
+		if(error){
+			res.send("An error was thrown: "+error.message);
+		} else {
+			//save and do something
+			res.redirect('/fin?token=' + accessToken);
 		}
 	});
 });
 
 app.get('/fin', function(req, res){
-	res.send(req.cookie('token')+' was my auth token');
+	res.send(req.query.token+'was my auth token');
 });
 
 app.get('/', function(req, res){
@@ -45,5 +44,4 @@ app.get('/', function(req, res){
 app.get('/team/:name', function(req, res){
 	res.send('Hello Team: ' + req.params.name);
 });
-*/
 app.listen(8080);
