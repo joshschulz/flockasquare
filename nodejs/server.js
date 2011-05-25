2f5b016d23a4d5dacb56cff9eaa23a46d86e3e72
josh <josh@schulzone.org>
2011-05-25 04:19:10 +0000

    Switching to express per irc recomendation

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,7 +1,14 @@
-var http = require('http');
+/*var http = require('http');
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hellow World\n');
 }).listen(8080);
+*/
 
+var app = express.createServer();
+app.get('/', function(req, res){
+	res.send('Hello new world');
+});
+
+app.listen(8080);
